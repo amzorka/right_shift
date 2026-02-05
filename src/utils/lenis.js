74b81525ab5d8eaee25d1b8b5966717803cd92a7
amzorka <salbiev62@mail.ru>
2026-02-05 20:48:43 +0300

    lenis fix

--- src/utils/lenis.js
+++ src/utils/lenis.js
@@ -13,7 +13,7 @@ export function initSmoothScroll() {
     if (lenis) return; // уже включен
 
     lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.3,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       smoothTouch: false,
